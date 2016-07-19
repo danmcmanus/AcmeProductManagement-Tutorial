89c8a5d69b1e3f4d58155ab0bd3814d7cea8470f
Dan McManus <danmcmanus11@outlook.com>
2016-07-19 14:05:43 -0500

    refined product detail component

--- app/products/product-detail.component.js
+++ app/products/product-detail.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', './product.service', '../shared/star.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1;
+    var core_1, router_1, product_service_1, star_component_1;
     var ProductDetailComponent;
     return {
         setters:[
@@ -19,24 +19,45 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (product_service_1_1) {
+                product_service_1 = product_service_1_1;
+            },
+            function (star_component_1_1) {
+                star_component_1 = star_component_1_1;
             }],
         execute: function() {
             ProductDetailComponent = (function () {
-                function ProductDetailComponent(_routeParams, _router) {
+                function ProductDetailComponent(_routeParams, _router, _productService) {
                     this._routeParams = _routeParams;
                     this._router = _router;
+                    this._productService = _productService;
                     this.pageTitle = "Product Detail";
-                    var id = +this._routeParams.get('id');
-                    this.pageTitle += ": " + id;
+                    //    let id = +this._routeParams.get('id');
+                    //    this.pageTitle += `: ${id}`;
                 }
+                //added
+                ProductDetailComponent.prototype.ngOnInit = function () {
+                    if (!this.product) {
+                        var id = +this._routeParams.get('id');
+                        this.getProduct(id);
+                    }
+                };
+                //added    
+                ProductDetailComponent.prototype.getProduct = function (id) {
+                    var _this = this;
+                    this._productService.getProduct(id)
+                        .subscribe(function (product) { return _this.product = product; }, function (error) { return _this.errorMessage = error; });
+                };
                 ProductDetailComponent.prototype.onBack = function () {
                     this._router.navigate(['Products']);
                 };
                 ProductDetailComponent = __decorate([
                     core_1.Component({
-                        templateUrl: 'app/products/product-detail.component.html'
+                        templateUrl: 'app/products/product-detail.component.html',
+                        directives: [star_component_1.StarComponent]
                     }), 
-                    __metadata('design:paramtypes', [router_1.RouteParams, router_1.Router])
+                    __metadata('design:paramtypes', [router_1.RouteParams, router_1.Router, product_service_1.ProductService])
                 ], ProductDetailComponent);
                 return ProductDetailComponent;
             }());
--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,16 +1,41 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
 import { RouteParams, Router } from 'angular2/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { StarComponent } from '../shared/star.component';
 
 @Component({
-    templateUrl: 'app/products/product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html',
+    directives: [StarComponent]
 })
 
 export class ProductDetailComponent {
     pageTitle: string = "Product Detail";
+    product: IProduct;
+    errorMessage: string;
 
-    constructor(private _routeParams: RouteParams, private _router: Router) {
-        let id = +this._routeParams.get('id');
-        this.pageTitle += `: ${id}`;
+    constructor(private _routeParams: RouteParams,
+        private _router: Router,
+        private _productService: ProductService) {
+    //    let id = +this._routeParams.get('id');
+    //    this.pageTitle += `: ${id}`;
+    }
+
+//added
+    ngOnInit() {
+       if (!this.product) {
+           let id = +this._routeParams.get('id');
+           this.getProduct(id);
+       } 
+    }
+
+//added    
+    getProduct(id: number) {
+        this._productService.getProduct(id)
+            .subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error
+            );
     }
 
     onBack(): void {
